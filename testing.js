5a48a4a7c5b1fd66a8fd4c578e59949f800b9210
Jason Gillanders <jgilland@ualberta.ca>
2023-07-06 22:06:47 -0600

    trying to create a wait/timeout function to allow all api calls to be made with no errors

--- testing.js
+++ testing.js
@@ -1,3 +1,4 @@
+eachYearofGames = [];
 
 async function testFetch() {
     result = await fetch("https://statsapi.web.nhl.com/api/v1/seasons");
@@ -10,25 +11,62 @@ async function testFetch() {
 }
 
 async function getSeason(seasonID) {
+    var input = document.getElementById("yearInput");
+    //var seasonID = input.value;
+    console.log("INPUT TEXT: " + seasonID);
     season = await fetch("https://statsapi.web.nhl.com/api/v1/schedule?season=" + seasonID);
     season.json().then(json => {
+        var newYear = new YearofGames();
         for (var i=0; i<json.dates.length; i++) {
             for (var j=0; j<json.dates[i].games.length; j++) {
                 var gameString = json.dates[i].games[j].link;
                 // i cant just do this because it calls the API too many times too fast
                 // need to find a way to not overload the system
-                getGame(gameString);
+                setTimeout(() => { 
+                    getGame(gameString, newYear);
+                }, 5000);
             }
-            break;
+            //break;
         }
+        eachYearofGames.push(newYear);
     });
 }
 
-async function getGame(gameLink) {
+async function getGame(gameLink, gameYear) {
     game = await fetch("https://statsapi.web.nhl.com" + gameLink);
-    season.json().then(json => {
-        console.log(json);
+    game.json().then(json => {
+        gameYear.addGame(json);
+        //console.log(json);
     });
+    
+}
+// continue trying this from https://stackoverflow.com/questions/33289726/combination-of-async-function-await-settimeout
+function timeout(ms) {
+
+}
+
+function printAllYears() {
+    for (var i in eachYearofGames) {
+        console.log(i);
+    }
+}
+
+class YearofGames {
+	constructor() {
+		this.games = [];
+	}
+    getGames() {
+        return this.games;
+    }
+    addGame(newGame) {
+        this.games.push(newGame);
+    }
+}
+
+class Game {
+    constructor(game) {
+        this.game = game;
+    }
 }
 
 testFetch();
