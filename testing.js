e23b0f2b7f279e97601f8c7bcbbb9c3683acbf3c
Jason Gillanders <jgilland@ualberta.ca>
2023-07-07 22:44:42 -0600

    got every single game in 2022-2023 with delay, no errors, has all players ID and who they are

--- testing.js
+++ testing.js
@@ -1,53 +1,62 @@
 eachYearofGames = [];
 
-async function testFetch() {
-    result = await fetch("https://statsapi.web.nhl.com/api/v1/seasons");
-    result.json().then(json => {
-        for (var i=0; i<json.seasons.length; i++) {
-            var seasonString = json.seasons[i].seasonId;
+
+function testFetch() {
+    axios.get('https://statsapi.web.nhl.com/api/v1/seasons')
+		.then((response) => {
+            //console.log(response.data);
+            /*
+        for (var i=0; i<response.data.seasons.length; i++) {
+            var seasonString = response.data.seasons[i].seasonId;
             getSeason(seasonString);
         }
+        */
+        getSeason('20222023');
+        printAllYears();
     });
 }
 
-async function getSeason(seasonID) {
+function getSeason(seasonID) {
     var input = document.getElementById("yearInput");
     //var seasonID = input.value;
     console.log("INPUT TEXT: " + seasonID);
-    season = await fetch("https://statsapi.web.nhl.com/api/v1/schedule?season=" + seasonID);
-    season.json().then(json => {
+    axios.get("https://statsapi.web.nhl.com/api/v1/schedule?season=" + seasonID)
+        .then(async (response) => {
         var newYear = new YearofGames();
-        for (var i=0; i<json.dates.length; i++) {
-            for (var j=0; j<json.dates[i].games.length; j++) {
-                var gameString = json.dates[i].games[j].link;
+        for (var i=1; i<response.data.dates.length; i++) {
+            for (var j=0; j<response.data.dates[i].games.length; j++) {
+                
+                //fetchGameData("https://statsapi.web.nhl.com" + json.dates[i].games[j].link);
+                
+                var gameString = response.data.dates[i].games[j].link;
                 // i cant just do this because it calls the API too many times too fast
                 // need to find a way to not overload the system
-                setTimeout(() => { 
-                    getGame(gameString, newYear);
-                }, 5000);
+                await sleep(100);
+                getGame(gameString, newYear);
             }
-            //break;
+            //if (i>2) break;
         }
         eachYearofGames.push(newYear);
     });
 }
 
-async function getGame(gameLink, gameYear) {
-    game = await fetch("https://statsapi.web.nhl.com" + gameLink);
-    game.json().then(json => {
-        gameYear.addGame(json);
-        //console.log(json);
+function getGame(gameLink, gameYear) {
+    axios.get("https://statsapi.web.nhl.com" + gameLink)
+        .then((response) => {
+        gameYear.addGame(response.data);
+        console.log(response.data);
     });
-    
 }
-// continue trying this from https://stackoverflow.com/questions/33289726/combination-of-async-function-await-settimeout
-function timeout(ms) {
 
+//Sleep please
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
+
 
 function printAllYears() {
     for (var i in eachYearofGames) {
-        console.log(i);
+        console.log(eachYearofGames[i].getGames());
     }
 }
 
